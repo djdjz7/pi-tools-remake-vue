aca236d566a32445b09cc1c7d8a05abd66d711e3
djdjz7 <djdjz7@icloud.com>
2024-08-18 14:06:52 +0800

    fix: hide infinity score

--- src/views/CalculatorView.vue
+++ src/views/CalculatorView.vue
@@ -8,7 +8,7 @@ const score = computed(() => {
   return (
     (20000000 * (4 - medicine.value) * (1 + multiplier.value)) /
     (3 * dayCount.value)
-  ).toFixed(0);
+  );
 });
 </script>
 
@@ -27,18 +27,18 @@ const score = computed(() => {
       <span m-r-2>分倍率</span>
       <input flex-grow-1 type="number" v-model="multiplier" />
     </div>
-    <span font-bold text-xl>分数: {{ score }}</span>
+    <span v-show="Number.isFinite(score)" font-bold text-xl>分数: {{ score.toFixed(0) }}</span>
   </div>
 </template>
 
 <style scoped>
 input {
   font-family: Inter;
-  @apply p-y-2 p-x-3 border-1 border-solid border-gray/40 rounded-2 dark:bg-dark;
-  @apply transition-all duration-200;
-  @apply focus:ring-2 ring-offset-1 ring-gray/50;
-  @apply text-dark dark:text-light;
-  @apply hover:shadow-md;
+  --at-apply: p-y-2 p-x-3 border-1 border-solid border-gray/40 rounded-2 dark:bg-dark
+    transition-all duration-200
+    focus:ring-2 ring-offset-1 ring-gray/50
+   text-dark dark:text-light
+   hover:shadow-md;
 }
 input:focus {
   outline: none;
